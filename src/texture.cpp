cab38e393b3669b69fd1b8c6f439144d8de44724
Tabris05 <afiery1yt@gmail.com>
2024-02-18 19:52:09 -0800

    fixed not allocating additional texture storage for mipmap levels

--- src/texture.cpp
+++ src/texture.cpp
@@ -16,6 +16,8 @@ std::optional<GLuint64> Texture::handle() const {
 }
 
 Texture Texture::make2D(unsigned char* data, int width, int height, GLenum internalFormat, GLenum format, GLenum minFilter, GLenum magFilter, GLenum wrapS, GLenum wrapT) {
+	bool hasMipmaps = (minFilter & 0xFF00) == 0x2700;
+	
 	GLuint id;
 	glCreateTextures(GL_TEXTURE_2D, 1, &id);
 
@@ -24,10 +26,10 @@ Texture Texture::make2D(unsigned char* data, int width, int height, GLenum inter
 	glTextureParameteri(id, GL_TEXTURE_WRAP_S, wrapS);
 	glTextureParameteri(id, GL_TEXTURE_WRAP_S, wrapT);
 
-	glTextureStorage2D(id, 1, internalFormat, width, height);
+	glTextureStorage2D(id, hasMipmaps ? std::floor(std::log2(std::max(width, height))) + 1 : 1, internalFormat, width, height);
 	if(data) glTextureSubImage2D(id, 0, 0, 0, width, height, format, GL_UNSIGNED_BYTE, data);
 
-	if ((minFilter & 0xFF00) == 0x2700) {
+	if (hasMipmaps) {
 		glGenerateTextureMipmap(id);
 		float anisotropy;
 		glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY, &anisotropy);
